b2ea248161a415ade365d4a4b9721a921daf7652
krubottom <karl.rubottom@gmail.com>
2017-11-04 22:59:02 -0600

    Cleanup

--- app.py
+++ app.py
@@ -5,11 +5,31 @@ from datetime import datetime
 
 db = TinyDB('rfid.json')
 
-#tag = raw_input("ID: ")
-db_date = date.today()
-db_time = datetime.time(datetime.now())
+def save_tag(tag):
+    db_date = date.today()
+    db_time = datetime.time(datetime.now())
+    db.insert({'rfid': tag, 'date': str(db_date), 'time': str(db_time)})
 
-#db.insert({'rfid': tag, 'date': str(db_date), 'time': str(db_time)})
+def get_tags():
+    for tag in db.all():
+        print(tag['rfid'])
 
-for tag in db.all():
-    print(tag['rfid'])
+def print_menu():
+    print "-" * 30
+    print "1) Save Tag"
+    print "2) List All Tags"
+    print "3) Exit"
+
+if __name__ == "__main__":
+    while True:
+        print_menu()
+        action = raw_input("> ")
+        if action == "1":
+            tag = raw_input("ID: ")
+            save_tag(tag)
+        elif action == "2":
+            get_tags()
+        elif action == "3":
+            exit()
+        else:
+            print "Bad Command"
