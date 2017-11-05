905c51f0ad0fde2bd4bf7d0b1f2f5267a9184b04
krubottom <karl.rubottom@gmail.com>
2017-11-04 23:10:36 -0600

    Updates

--- app.py
+++ app.py
@@ -12,13 +12,20 @@ def save_tag(tag):
 
 def get_tags():
     for tag in db.all():
-        print(tag['rfid'])
+        print(tag['rfid']) #Cleanup to return array
 
 def print_menu():
     print "-" * 30
     print "1) Save Tag"
     print "2) List All Tags"
-    print "3) Exit"
+    print "3) Convert to Binary"
+    print "0) Exit"
+
+def convert_to_rfid_bin(fc,num):
+    bin_fc = str(bin(fc))[2:]
+    bin_num = str(bin(num))[2:]
+    print "FC: " + bin_fc
+    print "Num: " + bin_num
 
 if __name__ == "__main__":
     while True:
@@ -30,6 +37,10 @@ if __name__ == "__main__":
         elif action == "2":
             get_tags()
         elif action == "3":
+            fc = int(raw_input("FC: "))
+            num = int(raw_input("Number: "))
+            convert_to_rfid_bin(fc,num)
+        elif action == "0":
             exit()
         else:
             print "Bad Command"
